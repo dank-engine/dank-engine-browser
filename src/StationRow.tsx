3a33dee8abbc31dd0b11ad1344152ef8adfca1bc
Kenton Lam <kenton_lam@outlook.com>
2019-03-16 17:39:36 +1000

    Animations.

--- src/StationRow.tsx
+++ src/StationRow.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 
 import {LEDMode} from './LEDMode';
+import './StationRow.css';
 
 export interface IProps {
     stationName: string;
@@ -19,7 +20,7 @@ class StationRow extends React.Component<IProps> {
   public render() {
     return (
       <div className="station">
-        <span className="station-led" style={{color: this.props.colour, opacity: this.props.opacity}}>●</span>
+        <span className={"station-led" + (this.props.mode===LEDMode.FLASH ? " station-flash" : "")} style={{color: this.props.colour, opacity: this.props.opacity}}>●</span>
         <span className="station-name">{this.props.stationName}</span>
       </div>
     );
